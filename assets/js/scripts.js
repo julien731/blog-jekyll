fc8f2dd23ec5882e5a9d257f60658104c00b85f1
Julien Liabeuf <julien@liabeuf.fr>
2018-03-31 13:33:30 +0700

    Add Ajax submission to contact form

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,14 +1,36 @@
 // A $( document ).ready() block.
-$( document ).ready(function() {
+$(document).ready(function () {
 
-	// Responsive-Nav
-	var nav = responsiveNav(".nav-collapse");
-
-	// Round Reading Time
+    // Round Reading Time
     $(".time").text(function (index, value) {
-      return Math.round(parseFloat(value));
+        return Math.round(parseFloat(value));
+    });
+
+    // Contact form.
+    var $contactForm = $('#contact-form');
+    $contactForm.submit(function (e) {
+        e.preventDefault();
+
+        $.ajax({
+            url: '//formspree.io/julien@liabeuf.fr',
+            method: 'POST',
+            data: $(this).serialize(),
+            dataType: 'json',
+            beforeSend: function () {
+                $contactForm.addClass("disabled");
+                $contactForm.find('.alert').remove();
+                $("#form-submit-button").attr("disabled", true).attr('value', 'Sending...');
+            },
+            success: function (data) {
+                $contactForm.append('<div class="alert alert-success">Message sent!</div>');
+                $("#form-submit-button").attr("disabled", true).attr('value', 'Sent!');
+            },
+            error: function (err) {
+                $contactForm.removeClass("disabled");
+                $("#form-submit-button").attr("disabled", false).attr('value', 'Send');
+                $contactForm.append('<div class="alert alert-error"><strong>Sorry, there was an error with your submission.</strong> Please make sure all required fields have been completed and try again.</div>');
+            }
+        });
     });
 
 });
-
-
--- contact.md
+++ contact.md
@@ -9,13 +9,13 @@ title: Contact Long Haul
     <p>Want to get in touch? Fill out the form below to send me a message and I will get back to you as soon as possible.</p>
     <p>If you need code review or consulting services, please note that I charge $100 US per hour with a minimum of 1 hour.</p>
   </div>
-  <form action="http://formspree.io/{{ site.social.email }}" method="POST">
+  <form action="http://formspree.io/{{ site.social.email }}" method="post" id="contact-form">
     <label for="name">Name</label>
     <input type="text" id="name" name="name" class="full-width"><br>
     <label for="email">Email Address</label>
     <input type="email" id="email" name="_replyto" class="full-width"><br>
     <label for="message">Message</label>
     <textarea name="message" id="message" cols="30" rows="10" class="full-width"></textarea><br>
-    <input type="submit" value="Send" class="button">
+    <input type="submit" id="form-submit-button" value="Send" class="button">
   </form>
 </div>
